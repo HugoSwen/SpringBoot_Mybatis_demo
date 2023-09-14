66137ebfe1545166eeb226495690023b84a8d662
HugoSwen <2407475450@qq.com>
2023-09-14 14:45:07 +0800

    修改注释

--- src/main/java/com/hugo/service/impl/DeptServiceImpl.java
+++ src/main/java/com/hugo/service/impl/DeptServiceImpl.java
@@ -3,8 +3,10 @@ package com.hugo.service.impl;
 import com.hugo.mapper.DeptMapper;
 import com.hugo.pojo.Dept;
 import com.hugo.service.DeptService;
+import com.hugo.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,14 +17,19 @@ public class DeptServiceImpl implements DeptService {
     @Autowired
     private DeptMapper deptMapper;
 
+    @Autowired
+    private EmpService empService;
+
     @Override
     public List<Dept> list() {
         return deptMapper.getList();
     }
 
+    @Transactional
     @Override
     public void delete(Integer id) {
         deptMapper.deleteById(id);
+        empService.deleteByDeptId(id);
     }
 
     @Override
